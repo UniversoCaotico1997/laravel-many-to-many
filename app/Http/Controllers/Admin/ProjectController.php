d1f8a9560e6c7a54794150991fc0566d4cdbc2c7
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-01-19 18:24:45 +0100

    Add scaffolding

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use App\Models\Type;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::orderByDesc('id')->get();
+
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::all();
+        // dd($type);
+        return view('admin.projects.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProjectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        // Visualizziamo la presenza dei dati
+        // dd($request->all());
+
+        // validiamo i dati
+        $val_data = $request->validated();
+
+
+        // check if
+        if ($request->hasFile('cover_image')) {
+            // Import img in storage/app/public
+            $cover_image = Storage::put('uploads', $val_data['cover_image']);
+
+            // check img
+            // dd($cover_image);
+
+            // Img Validate
+            $val_data['cover_image'] = $cover_image;
+        }
+
+
+        // Creazione slug
+        $project_slug = Str::slug($val_data['title']);
+        // dd($project_slug);
+        $val_data['slug'] = $project_slug;
+        // dd($val_data);
+
+
+        // // Creazione dei nuovi dati validati
+        $project = Project::create($val_data);
+
+        // Ritorniamo allo rotta index
+        return to_route('admin.projects.index')->with('message', "Project id: $project->id added successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+        return view('admin.projects.edit', compact('project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectRequest  $request
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        // Visualizziamo al presenza dei dati
+        // dd($request->all());
+
+        // validiamo i dati
+        $val_data = $request->validated();
+        // dd($val_data);
+
+        // check if
+        if ($request->hasFile('cover_image')) {
+            if ($project->cover_image) {
+                Storage::delete($project->cover_image);
+            }
+            // Import img in storage/app/public
+            $cover_image = Storage::put('uploads', $val_data['cover_image']);
+
+            // check img
+            // dd($cover_image);
+
+            // Img Validate
+            $val_data['cover_image'] = $cover_image;
+        }
+
+        // Creazione slug
+        $project_slug = Str::slug($val_data['title']);
+        // dd($project_slug);
+
+        $val_data['slug'] = $project_slug;
+        // dd($val_data);
+
+        // Creazione  Project
+        $project->update($val_data);
+
+        // Ritorniamo allo rotta index
+        return to_route('admin.projects.index')->with('message', "Project id: $project->id update successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return to_route('admin.projects.index')->with('message', "Project id: $project->id Deleted successfully");
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\App;
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project;
+            $project->title = $faker->sentence(3);
+            $project->slug = Str::slug($project->title, '-');
+            $project->cover_image = 'placeholders/' . $faker->image('storage/app/public/placeholders', 600, 300, 'Project', false, false);
+            $project->description = $faker->text(255);
+            $project->save();
+        }
+    }
+}
